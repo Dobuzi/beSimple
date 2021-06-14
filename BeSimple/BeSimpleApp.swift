62d4e7e59708af177196765f766aa006187054a5
Dobuzi <kimjw7981@gmail.com>
2021-06-14 14:47:53 +0900

    Initial Commit

---
+++ BeSimple/BeSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeSimpleApp.swift
+//  BeSimple
+//
+//  Created by 김종원 on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct BeSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeSimple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeSimple
+//
+//  Created by 김종원 on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
